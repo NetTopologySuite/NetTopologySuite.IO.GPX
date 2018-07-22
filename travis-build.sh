0a70f59ae1f29b57477856f6af2f3fed829b72be
Joe Amenta <airbreather@linux.com>
2018-07-22 12:27:39 -0400

    this would be easier if I were developing on Linux...

--- travis-build.sh
+++ travis-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 dotnet build -c Release
 dotnet test NetTopologySuite.IO.GPX.Tests --no-build --no-restore -c Release
-if [ "$TRAVIS_EVENT_TYPE" = "push" && "$TRAVIS_BRANCH" == "develop" ]
+if [ "$TRAVIS_EVENT_TYPE" = "push" ] && [ "$TRAVIS_BRANCH" = "develop" ]
 then
     dotnet pack --no-build --no-dependencies --version-suffix=travis$(printf "%05d" $TRAVIS_BUILD_NUMBER) --output $TRAVIS_BUILD_DIR -c Release
     dotnet nuget push *.nupkg -k $MYGET_API_KEY -s https://www.myget.org/F/airbreather/api/v2/package
