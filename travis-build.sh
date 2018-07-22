b2d1601e570c3ed03eb59caa418f1edc93c964bf
Joe Amenta <airbreather@linux.com>
2018-07-22 11:46:44 -0400

    Try to get travis pushing packages.

---
+++ travis-build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+dotnet build -c Release
+dotnet test NetTopologySuite.IO.GPX.Tests --no-build --no-restore -c Release
+dotnet pack --no-build --no-dependencies --version-suffix=-pre$(printf \"%05d\" $TRAVIS_BUILD_NUMBER) -c Release
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]
+then
+    dotnet nuget push *.nupkg -k $MYGET_API_KEY -s https://www.myget.org/F/airbreather/api/v2/package
+fi
