450af4261015da7a1a234d6be2557ed64f2683ee
Joe Amenta <airbreather@linux.com>
2018-07-22 12:24:07 -0400

    Remove the extra dash "pre" to "travis" only do "dotnet pack" when triggered by a push to "develop"

--- travis-build.sh
+++ travis-build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 dotnet build -c Release
 dotnet test NetTopologySuite.IO.GPX.Tests --no-build --no-restore -c Release
-dotnet pack --no-build --no-dependencies --version-suffix=-pre$(printf \"%05d\" $TRAVIS_BUILD_NUMBER) --output $TRAVIS_BUILD_DIR -c Release
-if [ "$TRAVIS_PULL_REQUEST" = "false" ]
+if [ "$TRAVIS_EVENT_TYPE" = "push" && "$TRAVIS_BRANCH" == "develop" ]
 then
+    dotnet pack --no-build --no-dependencies --version-suffix=travis$(printf "%05d" $TRAVIS_BUILD_NUMBER) --output $TRAVIS_BUILD_DIR -c Release
     dotnet nuget push *.nupkg -k $MYGET_API_KEY -s https://www.myget.org/F/airbreather/api/v2/package
 fi
