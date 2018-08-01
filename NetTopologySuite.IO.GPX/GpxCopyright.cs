51d7ee446c3d3970bae982d9b4188efd49fde22f
Joe Amenta <airbreather@linux.com>
2018-08-01 08:15:37 -0400

    xmldoc for GpxCopyright

--- NetTopologySuite.IO.GPX/GpxCopyright.cs
+++ NetTopologySuite.IO.GPX/GpxCopyright.cs
@@ -5,8 +5,29 @@ using System.Xml.Linq;
 
 namespace NetTopologySuite.IO
 {
+    /// <summary>
+    /// Represents copyright-related information.
+    /// </summary>
+    /// <remarks>
+    /// In the official XSD schema for GPX 1.1, this corresponds to the complex type "<a href="http://www.topografix.com/GPX/1/1/#type_copyrightType">copyrightType</a>".
+    /// </remarks>
     public sealed class GpxCopyright : ICanWriteToXmlWriter
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GpxCopyright"/> class.
+        /// </summary>
+        /// <param name="year">
+        /// The value for <see cref="Year"/>.
+        /// </param>
+        /// <param name="licenseUri">
+        /// The value for <see cref="LicenseUri"/>.
+        /// </param>
+        /// <param name="author">
+        /// The value for <see cref="Author"/>.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="author"/> is <see langword="null" />.
+        /// </exception>
         public GpxCopyright(int? year, Uri licenseUri, string author)
         {
             this.Year = year;
@@ -14,7 +35,45 @@ namespace NetTopologySuite.IO
             this.Author = author ?? throw new ArgumentNullException(nameof(author));
         }
 
-        public static GpxCopyright Load(XElement element)
+        /// <summary>
+        /// Gets an optional Gregorian year for the copyright.
+        /// </summary>
+        /// <remarks>
+        /// <para>
+        /// In the official XSD schema for GPX 1.1, this corresponds to the "year" element.
+        /// </para>
+        /// <para>
+        /// This value is not completely round-trip safe.  In GPX 1.1, its type is "gYear", which
+        /// would technically support values less than <see cref="int.MinValue"/> and greater than
+        /// <see cref="int.MaxValue"/>.  Less pedantically, "gYear" also supports an optional offset
+        /// from UTC.  When loading from XML, a time zone specifier (if present) is observed,
+        /// validated, and then ignored.
+        /// </para>
+        /// </remarks>
+        public int? Year { get; }
+
+        /// <summary>
+        /// Gets an optional <see cref="Uri"/> that identifies the copyright license.
+        /// </summary>
+        /// <remarks>
+        /// In the official XSD schema for GPX 1.1, this corresponds to the "license" element.
+        /// </remarks>
+        public Uri LicenseUri { get; }
+
+        /// <summary>
+        /// Gets the author who holds the copyright.
+        /// </summary>
+        /// <remarks>
+        /// In the official XSD schema for GPX 1.1, this corresponds to the "author" attribute.
+        /// </remarks>
+        public string Author { get; }
+
+        /// <inheritdoc />
+        public override string ToString() => Helpers.BuildString((nameof(this.Year), this.Year),
+                                                                 (nameof(this.LicenseUri), this.LicenseUri),
+                                                                 (nameof(this.Author), this.Author));
+
+        internal static GpxCopyright Load(XElement element)
         {
             if (element is null)
             {
@@ -27,21 +86,11 @@ namespace NetTopologySuite.IO
                 author: element.Attribute("author")?.Value ?? throw new XmlException("copyright element must have author attribute."));
         }
 
-        public void Save(XmlWriter writer)
+        void ICanWriteToXmlWriter.Save(XmlWriter writer)
         {
             writer.WriteAttributeString("author", this.Author);
             writer.WriteOptionalGpxElementValue("year", this.Year?.ToString("0000", CultureInfo.InvariantCulture));
             writer.WriteOptionalGpxElementValue("license", this.LicenseUri?.OriginalString);
         }
-
-        public int? Year { get; }
-
-        public Uri LicenseUri { get; }
-
-        public string Author { get; }
-
-        public override string ToString() => Helpers.BuildString((nameof(this.Year), this.Year),
-                                                                 (nameof(this.LicenseUri), this.LicenseUri),
-                                                                 (nameof(this.Author), this.Author));
     }
 }
