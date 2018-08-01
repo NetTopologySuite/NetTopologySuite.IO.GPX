2e310457c46abb9f88f443b5d99cf70905326a4f
Joe Amenta <airbreather@linux.com>
2018-08-01 08:02:13 -0400

    xmldoc for GpxBoundingBox

--- NetTopologySuite.IO.GPX/GpxBoundingBox.cs
+++ NetTopologySuite.IO.GPX/GpxBoundingBox.cs
@@ -4,8 +4,29 @@ using System.Xml.Linq;
 
 namespace NetTopologySuite.IO
 {
+    /// <summary>
+    /// Represents two lat/lon pairs defining the extent of an element.
+    /// </summary>
+    /// <remarks>
+    /// In the official XSD schema for GPX 1.1, this corresponds to the complex type "<a href="http://www.topografix.com/GPX/1/1/#type_boundsType">boundsType</a>".
+    /// </remarks>
     public sealed class GpxBoundingBox : ICanWriteToXmlWriter
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GpxBoundingBox"/> class.
+        /// </summary>
+        /// <param name="minLongitude">
+        /// The value for <see cref="MinLongitude"/>.
+        /// </param>
+        /// <param name="minLatitude">
+        /// The value for <see cref="MinLatitude"/>.
+        /// </param>
+        /// <param name="maxLongitude">
+        /// The value for <see cref="MaxLongitude"/>.
+        /// </param>
+        /// <param name="maxLatitude">
+        /// The value for <see cref="MaxLatitude"/>.
+        /// </param>
         public GpxBoundingBox(GpxLongitude minLongitude, GpxLatitude minLatitude, GpxLongitude maxLongitude, GpxLatitude maxLatitude)
         {
             this.MinLongitude = minLongitude;
@@ -14,7 +35,45 @@ namespace NetTopologySuite.IO
             this.MaxLatitude = maxLatitude;
         }
 
-        public static GpxBoundingBox Load(XElement element)
+        /// <summary>
+        /// Gets the minimum <see cref="GpxLongitude"/> value.
+        /// </summary>
+        /// <remarks>
+        /// In the official XSD schema for GPX 1.1, this corresponds to the "minlon" attribute.
+        /// </remarks>
+        public GpxLongitude MinLongitude { get; }
+
+        /// <summary>
+        /// Gets the minimum <see cref="GpxLatitude"/> value.
+        /// </summary>
+        /// <remarks>
+        /// In the official XSD schema for GPX 1.1, this corresponds to the "minlat" attribute.
+        /// </remarks>
+        public GpxLatitude MinLatitude { get; }
+
+        /// <summary>
+        /// Gets the maximum <see cref="GpxLongitude"/> value.
+        /// </summary>
+        /// <remarks>
+        /// In the official XSD schema for GPX 1.1, this corresponds to the "maxlon" attribute.
+        /// </remarks>
+        public GpxLongitude MaxLongitude { get; }
+
+        /// <summary>
+        /// Gets the maximum <see cref="GpxLatitude"/> value.
+        /// </summary>
+        /// <remarks>
+        /// In the official XSD schema for GPX 1.1, this corresponds to the "maxlat" attribute.
+        /// </remarks>
+        public GpxLatitude MaxLatitude { get; }
+
+        /// <inheritdoc />
+        public override string ToString() => Helpers.BuildString((nameof(this.MinLongitude), this.MinLongitude),
+                                                                 (nameof(this.MinLatitude), this.MinLatitude),
+                                                                 (nameof(this.MaxLongitude), this.MaxLongitude),
+                                                                 (nameof(this.MaxLatitude), this.MaxLatitude));
+
+        internal static GpxBoundingBox Load(XElement element)
         {
             if (element is null)
             {
@@ -28,25 +87,12 @@ namespace NetTopologySuite.IO
                 maxLatitude: Helpers.ParseLatitude(element.Attribute("maxlat")?.Value) ?? throw new XmlException("bounds element must have maxlat attribute"));
         }
 
-        public void Save(XmlWriter writer)
+        void ICanWriteToXmlWriter.Save(XmlWriter writer)
         {
             writer.WriteAttributeString("minlat", this.MinLatitude.Value.ToRoundTripString(CultureInfo.InvariantCulture));
             writer.WriteAttributeString("minlon", this.MinLongitude.Value.ToRoundTripString(CultureInfo.InvariantCulture));
             writer.WriteAttributeString("maxlat", this.MaxLatitude.Value.ToRoundTripString(CultureInfo.InvariantCulture));
             writer.WriteAttributeString("maxlon", this.MaxLongitude.Value.ToRoundTripString(CultureInfo.InvariantCulture));
         }
-
-        public GpxLongitude MinLongitude { get; }
-
-        public GpxLatitude MinLatitude { get; }
-
-        public GpxLongitude MaxLongitude { get; }
-
-        public GpxLatitude MaxLatitude { get; }
-
-        public override string ToString() => Helpers.BuildString((nameof(this.MinLongitude), this.MinLongitude),
-                                                                 (nameof(this.MinLatitude), this.MinLatitude),
-                                                                 (nameof(this.MaxLongitude), this.MaxLongitude),
-                                                                 (nameof(this.MaxLatitude), this.MaxLatitude));
     }
 }
