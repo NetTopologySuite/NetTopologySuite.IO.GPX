50921ef6f7aa0313d5e386841e618b1a4eb48d13
Joe Amenta <airbreather@linux.com>
2018-08-04 15:47:58 -0400

    Some niceness, now that I've got it working at all.

--- generate-docs.cmd
+++ generate-docs.cmd
@@ -1,13 +1,18 @@
 @echo off
+REM ===========================================================================
+REM Updates https://nettopologysuite.github.io/NetTopologySuite.IO.GPX to have
+REM the documentation that corresponds to whatever's in our working tree.
+REM ===========================================================================
+set DOCFX_PACKAGE_VERSION=2.37.2
 pushd %~dp0
 dotnet restore
 pushd tools
-rd /s /q docfx.console.2.37.2
-nuget install docfx.console -Version 2.37.2
+rd /s /q docfx.console.%DOCFX_PACKAGE_VERSION%
+nuget install docfx.console -Version %DOCFX_PACKAGE_VERSION%
 popd
 pushd doc
 rd /s /q artifacts obj
-%~dp0\tools\docfx.console.2.37.2\tools\docfx
+%~dp0\tools\docfx.console.%DOCFX_PACKAGE_VERSION%\tools\docfx
 pushd artifacts
 git clone --branch gh-pages %~dp0 gh-pages
 xcopy /Q /E /R /Y _site gh-pages
