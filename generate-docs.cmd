09b8b414c904442cdfb9e5629390f480844324e3
Joe Amenta <airbreather@linux.com>
2018-08-04 15:34:43 -0400

    Probably just do it directly.

--- generate-docs.cmd
+++ generate-docs.cmd
@@ -4,15 +4,18 @@ dotnet restore
 pushd tools
 rd /s /q docfx.console.2.37.2
 nuget install docfx.console -Version 2.37.2
-rd /s /q 7-Zip.CommandLine.18.1.0
-nuget install 7-Zip.CommandLine -Version 18.1.0
 popd
 pushd doc
 rd /s /q artifacts obj
 %~dp0\tools\docfx.console.2.37.2\tools\docfx
-del gh-pages.7z
-pushd artifacts\_site
-%~dp0\tools\7-Zip.CommandLine.18.1.0\tools\x64\7za a -r ..\..\gh-pages.7z .
+pushd artifacts
+git clone --branch gh-pages %~dp0 gh-pages
+xcopy _site gh-pages
+pushd gh-pages
+git add .
+git commit -m "Update docs.  This was performed automatically."
+git push
+popd
 popd
 popd
 popd
