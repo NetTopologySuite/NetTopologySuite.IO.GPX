a31b104f51dc14a1d212b5458d96a83e14fa4c4c
Joe Amenta <airbreather@linux.com>
2018-08-05 14:19:23 -0400

    Heh, we should also delete stuff that was deleted.

--- push-docs.cmd
+++ push-docs.cmd
@@ -7,8 +7,9 @@ pushd %~dp0
 pushd doc\obj
 rd /s /q gh-pages
 git clone --branch gh-pages %~dp0 gh-pages
-xcopy /Q /E /R /Y generated-site-content gh-pages
 pushd gh-pages
+git rm -r .
+xcopy /Q /E /R /Y ..\generated-site-content .
 git add .
 git commit -m "Update docs.  This was performed automatically."
 git push origin gh-pages
