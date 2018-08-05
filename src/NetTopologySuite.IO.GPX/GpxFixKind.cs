396ad9d4b34aaccf2744c7cf9dd9a33c71e05585
Joe Amenta <airbreather@linux.com>
2018-08-05 13:16:28 -0400

    xmldoc for GpxFixKind

--- src/NetTopologySuite.IO.GPX/GpxFixKind.cs
+++ src/NetTopologySuite.IO.GPX/GpxFixKind.cs
@@ -1,11 +1,33 @@
 ﻿namespace NetTopologySuite.IO
 {
+    /// <summary>
+    /// Specifies what kind of GPS fix a particular waypoint had.
+    /// </summary>
     public enum GpxFixKind
     {
+        /// <summary>
+        /// No fix.
+        /// </summary>
         None,
+
+        /// <summary>
+        /// 2D fix.
+        /// </summary>
         TwoDimensional,
+
+        /// <summary>
+        /// 3D fix.
+        /// </summary>
         ThreeDimensional,
+
+        /// <summary>
+        /// Differential GPS (DGPS) fix.
+        /// </summary>
         DGPS,
+
+        /// <summary>
+        /// Precise Positioning Service (PPS) fix.
+        /// </summary>
         PPS
     }
 }
