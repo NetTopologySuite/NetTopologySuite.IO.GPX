b99c802ba885cdc4562a1b97079cb21c582a6e6c
Joe Amenta <airbreather@linux.com>
2018-08-19 21:47:05 -0400

    Add a test that the default instance can technically be saved out to a file and read back in again.

--- tests/NetTopologySuite.IO.GPX.Tests/GpxFileTests.cs
+++ tests/NetTopologySuite.IO.GPX.Tests/GpxFileTests.cs
@@ -40,5 +40,29 @@ namespace NetTopologySuite.IO
         }
 
         public static object[][] RoundTripSafeSamples => Array.ConvertAll(Directory.GetFiles("RoundTripSafeSamples", "*.gpx", SearchOption.TopDirectoryOnly), fl => new object[] { fl });
+
+        [Fact]
+        public void DefaultInstanceShouldBeValid()
+        {
+            var file = new GpxFile();
+            using (var ms = new MemoryStream())
+            {
+                var writerSettings = new XmlWriterSettings { Encoding = Encoding.UTF8, CloseOutput = false };
+                using (var wr = XmlWriter.Create(ms, writerSettings))
+                {
+                    file.WriteTo(wr, null);
+                }
+
+                ms.Position = 0;
+                file = GpxFile.Read(XmlReader.Create(ms), null);
+                Assert.NotNull(file);
+                Assert.Equal("NetTopologySuite.IO.GPX", file.Metadata.Creator);
+                Assert.True(file.Metadata.IsTrivial);
+                Assert.Empty(file.Waypoints);
+                Assert.Empty(file.Routes);
+                Assert.Empty(file.Tracks);
+                Assert.Null(file.Extensions);
+            }
+        }
     }
 }
